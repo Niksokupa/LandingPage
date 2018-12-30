0ae5f8ba6a7dcff3388a12ad31110c1927e08b79
Ramón Lozano <niksokupa@gmail.com>
2018-12-30 17:02:27 +0100

    fadeout ciudades fadein login

--- myjs.js
+++ myjs.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
 
     $(function () {
+        $("#loginform").hide();
         $(".center").hide().delay(800).fadeIn(3000);
     });
+
+
+
+    $("#login").click(function () {
+        $('#ciudades').fadeToggle(1000, function () {
+            $('#ciudades').remove();
+        });
+        $('#loginform').delay(1200).fadeToggle(1000);
+
+    });
+
 });
